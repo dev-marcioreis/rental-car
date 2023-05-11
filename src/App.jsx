cf9bc1fc1596b20ac54448a1fb3686d1d4500724
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-05-11 13:57:43 -0300

    import Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import Navbar from './components/navbar/Navbar'
+import About from './pages/About'
+import Home from './pages/Home'
+import Models from './pages/Models'
+import TestimonialsPage from './pages/TestimonialsPage'
+import Team from './pages/Team'
+import Contact from './pages/Contact'
+
 import './App.css'
+
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      Em desenvolvimento...
+      <Navbar />
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='models' element={<Models />} />
+        <Route path='testimonials' element={<TestimonialsPage />} />
+        <Route path='team' element={<Team />} />
+        <Route path='contact' element={<Contact />} />
+      </Routes>
     </>
   )
 }
